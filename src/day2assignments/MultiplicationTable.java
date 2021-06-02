7ad47dd833b8452bb2803956e3876bf207fb0ca6
YunaH <YunaH503@gmail.com>
2021-06-02 16:14:39 -0400

    Created day2assignments, completed num1

---
+++ src/day2assignments/MultiplicationTable.java
@@ -0,0 +1,23 @@
+package day2assignments;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		
+		// creates an object of scanner
+		Scanner input = new Scanner(System.in);
+		
+		// User Input
+		System.out.println("Enter a number to multiply from 1 - 10: ");
+		int num = input.nextInt();
+		
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+        input.close();
+	}
+
+}
