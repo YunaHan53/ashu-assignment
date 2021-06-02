e832e3a556be9eaf06b84eaeaa3bc070ba0802f6
YunaH <YunaH503@gmail.com>
2021-06-02 16:38:05 -0400

    Completed assignment num3

---
+++ src/day2assignments/LowercaseAlphabet.java
@@ -0,0 +1,18 @@
+package day2assignments;
+
+/*
+ * 3. Display lowercase alphabet using for loop HINT -  for(c = 'a'; c <= 'z'; ++c)
+ */
+
+public class LowercaseAlphabet {
+
+	public static void main(String[] args) {
+
+		char c;
+		
+		for(c = 'a'; c <= 'z'; c++) {
+			System.out.println(c + " ");
+		}
+	}
+
+}
